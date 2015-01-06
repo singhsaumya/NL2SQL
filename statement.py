2b390d7fcdd637376dfb8885475aae18f8905d2f
dhruv sharma <dhruvshubham@gmail.com>
2015-01-06 19:36:36 +0530

    code-structure

--- statement.py
+++ statement.py
@@ -5,7 +5,36 @@ class Statement:
     NLStatement = ""
     schema = {}
     
-                     
+    def typeQuery(self):
+        self.keywords =['SELECT']
+        return
+    
+    def comparisonOperator(self):
+        return
+    
+    def preprocessor(self):
+        self.typeQuery()
+        self.comparisonOperator()
+        return
+    
+    def postprocessor(self):
+        return 
+    
+    def generateSQL(self):
+        return
+     
+    
+    def convert2SQL(self, nls=None):
+        if not nls:
+            self.NLStatement = nls
+        
+        self.prepocessor()
+        self.postprocessor()
+        self.generateSQL()
+        
+        return "SELECT * from Table"
+    
+    
     def getSchema(self, schema):
         if isinstance(schema, str):
             schema = json.loads(schema)
@@ -15,9 +44,4 @@ class Statement:
     
     def __init__(self, schema={}):
         self.schema = self.getSchema(schema)
-    def convert2SQL(self, nls=None):
-        if not nls:
-            self.NLStatement = nls
-        
-        return "SELECT * from Table"
-        
+            
